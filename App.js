7e0b921d04c8a7cd3f9471b1848878b1ab9062d4
Ryunosuke  Kanai <rk@RyunosukenoMacBook-Pro.local>
2020-01-11 16:34:49 +0900

    first commit

---
+++ App.js
@@ -0,0 +1,102 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={{ padding: 15, flexDirection: 'column' }}>
+
+
+      <View
+        style={{
+          backgroundColor: 'red',
+          width: 50,
+          height: 50,
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+        <Text>1</Text>
+      </View>
+
+
+
+      <View
+        style={{
+          backgroundColor: 'blue',
+          width: 50,
+          height: 50,
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+
+
+        <Text>2</Text>
+      </View>
+      <View
+        style={{
+          backgroundColor: 'green',
+          width: 50,
+          height: 50,
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+        <Text>3</Text>
+      </View>
+
+      <View
+        style={{
+          backgroundColor: 'purple',
+          width: 50,
+          height: 50,
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+        <Text>4</Text>
+      </View>
+      
+
+      <View
+        style={{
+          backgroundColor: 'orange',
+          width: 50,
+          height: 50,
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+        <Text>5</Text>
+      </View>
+      
+
+      <View
+        style={{
+          backgroundColor: 'grey',
+          width: 50,
+          height: 50,
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+        <Text>6</Text>
+      </View>
+
+
+      <View
+        style={{
+          backgroundColor: 'pink',
+          width: 50,
+          height: 50,
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+        <Text>7</Text>
+      </View>
+
+
+    </View>
+  );
+}

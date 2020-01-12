b41b0ae9f82d6d1a362797e6f51b46e92c5fc720
Ryunosuke  Kanai <rk@RyunosukenoMacBook-Pro.local>
2020-01-12 20:17:58 +0900

    42seats

--- App.js
+++ App.js
@@ -1,102 +1,141 @@
-import React from 'react';
-import { Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={{ padding: 15, flexDirection: 'column' }}>
+import React, { Component } from 'react';
+import { View, Text, Button } from 'react-native';
+
+export default class JustifyContentBasics extends Component {
+  render() {
+    return (
+      
+      <View style={{
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        padding:45
+      }}>
+        <View style={{
+
+          }}>
+            <Button title="seat1" onPress={()=>{}}/>
+
+            <Button title="seat2" onPress={()=>{}}/>
+
+            <Button title="seat3" onPress={()=>{}}/>
+
+            <Button title="seat4" onPress={()=>{}}/>
+
+            <Button title="seat5" onPress={()=>{}}/>
+
+            <Button title="seat6" onPress={()=>{}}/>
+
+            <Button title="seat7" onPress={()=>{}}/>
+            </View>
+
+            <View>
+            <Button title="seat8" onPress={()=>{}}/>
+
+            <Button title="seat9" onPress={()=>{}}/>
+
+            <Button title="seat10" onPress={()=>{}}/>
+
+            <Button title="seat11" onPress={()=>{}}/>
+
+            <Button title="seat12" onPress={()=>{}}/>
+
+            <Button title="seat13" onPress={()=>{}}/>
+
+            <Button title="seat14" onPress={()=>{}}/>
+
+            </View>
+
+            <View>
+
+            <Button title="seat15" onPress={()=>{}}/>
+
+            <Button title="seat16" onPress={()=>{}}/>
+
+            <Button title="seat17" onPress={()=>{}}/>
+
+            <Button title="seat18" onPress={()=>{}}/>
+
+            <Button title="seat19" onPress={()=>{}}/>
+
+            <Button title="seat20" onPress={()=>{}}/>
+
+            <Button title="seat21" onPress={()=>{}}/>
+
+            </View>
 
 
-      <View
-        style={{
-          backgroundColor: 'red',
-          width: 50,
-          height: 50,
-          justifyContent: 'center',
-          alignItems: 'center'
-        }}
-      >
-        <Text>1</Text>
+            <View>
+            
+
+            <Button title="seat22" onPress={()=>{}}/>
+
+            <Button title="seat23" onPress={()=>{}}/>
+
+            <Button title="seat24" onPress={()=>{}}/>
+
+            <Button title="seat25" onPress={()=>{}}/>
+
+            <Button title="seat26" onPress={()=>{}}/>
+
+            <Button title="seat27" onPress={()=>{}}/>
+
+            <Button title="seat28" onPress={()=>{}}/>
+
+            </View>
+
+
+            <View>
+            
+
+            <Button title="seat29" onPress={()=>{}}/>
+
+            <Button title="seat30" onPress={()=>{}}/>
+
+            <Button title="seat31" onPress={()=>{}}/>
+
+            <Button title="seat32" onPress={()=>{}}/>
+
+            <Button title="seat33" onPress={()=>{}}/>
+
+            <Button title="seat34" onPress={()=>{}}/>
+
+            <Button title="seat35" onPress={()=>{}}/>
+
+            </View>
+
+            <View>
+            
+
+            <Button title="seat36" onPress={()=>{}}/>
+
+            <Button title="seat37" onPress={()=>{}}/>
+
+            <Button title="seat38" onPress={()=>{}}/>
+
+            <Button title="seat39" onPress={()=>{}}/>
+
+            <Button title="seat40" onPress={()=>{}}/>
+
+            <Button title="seat41" onPress={()=>{}}/>
+
+            <Button title="seat42" onPress={()=>{}}/>
+
+            </View>
+
+
       </View>
 
 
 
-      <View
-        style={{
-          backgroundColor: 'blue',
-          width: 50,
-          height: 50,
-          justifyContent: 'center',
-          alignItems: 'center'
-        }}
-      >
-
-
-        <Text>2</Text>
-      </View>
-      <View
-        style={{
-          backgroundColor: 'green',
-          width: 50,
-          height: 50,
-          justifyContent: 'center',
-          alignItems: 'center'
-        }}
-      >
-        <Text>3</Text>
-      </View>
-
-      <View
-        style={{
-          backgroundColor: 'purple',
-          width: 50,
-          height: 50,
-          justifyContent: 'center',
-          alignItems: 'center'
-        }}
-      >
-        <Text>4</Text>
-      </View>
       
 
-      <View
-        style={{
-          backgroundColor: 'orange',
-          width: 50,
-          height: 50,
-          justifyContent: 'center',
-          alignItems: 'center'
-        }}
-      >
-        <Text>5</Text>
-      </View>
       
 
-      <View
-        style={{
-          backgroundColor: 'grey',
-          width: 50,
-          height: 50,
-          justifyContent: 'center',
-          alignItems: 'center'
-        }}
-      >
-        <Text>6</Text>
-      </View>
 
 
-      <View
-        style={{
-          backgroundColor: 'pink',
-          width: 50,
-          height: 50,
-          justifyContent: 'center',
-          alignItems: 'center'
-        }}
-      >
-        <Text>7</Text>
-      </View>
+    );
+  }
+};
 
 
-    </View>
-  );
-}
